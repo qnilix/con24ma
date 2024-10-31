f8c50b3321ab394a6205c7e1f7c8bebfb95318cb
qailN <kyamlinqa@gmail.com>
2024-10-31 13:24:51 +0900

    v0.2.0

--- con24ma/use_dataclasses/instype.py
+++ con24ma/use_dataclasses/instype.py
@@ -5,7 +5,8 @@
 class _Optional:
 
     def __getitem__(self, key: type):
-        def _w(value): return key(value)
+        def _w(value):
+            return key(value) if value is not None else None
         return _w
 
 
